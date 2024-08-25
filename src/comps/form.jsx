9f847f5578cecba0cf7f825caf1a7bd446a1986a
shweta281 <cse21310@glbitm.ac.in>
2024-08-26 00:28:01 +0530

    form

--- src/comps/form.jsx
+++ src/comps/form.jsx
@@ -1,7 +1,28 @@
+import { useState } from "react"
+import "./form.css"
+
 export default function form(){
+    const [ detail, setDetail ]= useState({ firstname:"", lastname:"", email:"", comments:""})
+
+    console.log(detail);
+
+    function eventHandle(e){
+        setDetail(()=> {
+            return {
+                ...detail,
+                [e.target.name]: e.target.value
+            }
+        } )
+    }
     return (
-        <div>
-            <h1>foram</h1>
+        <div className="form">
+            <form>
+                <h1>form</h1>
+                <input type="text" placeholder="First Name" onChange={ eventHandle } name="firstname" value={detail.firstname}/>
+                <input type="text" placeholder="Last Name" onChange={ eventHandle } name="lastname" value={detail.lastname}/>
+                <input type="email" placeholder="email" onChange={ eventHandle} name="email" value={detail.email}/>
+                <textarea value={detail.comments} name="comments" placeholder="Comments..." onChange={ eventHandle}/>
+            </form>
         </div>
     )
 }
